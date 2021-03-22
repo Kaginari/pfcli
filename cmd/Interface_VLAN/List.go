531ce94e209d027fe92f9e6c92ab34dc2135cb9f
NoussairHamrit <noussair.hamrit@allence-tunisie.com>
2021-03-22 14:41:58 +0100

    feat:Creation de le commande interface/vlan et ses sous commandes

---
+++ cmd/Interface_VLAN/List.go
@@ -0,0 +1,51 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package Interface_VLAN
+
+import (
+	"crypto/tls"
+	"fmt"
+	"github.com/spf13/cobra"
+	"gitlab.com/nt-factory/2021/admin/pfcli/functions"
+	"log"
+	"net/http"
+)
+
+// ListCmd represents the List command
+var ListCmd = &cobra.Command{
+	Use:   "list",
+	Run: func(cmd *cobra.Command, args []string) {
+		VlanList()
+	},
+}
+
+func init() {
+}
+func VlanList()  {
+	req, err := http.NewRequest("GET", functions.ViperReadConfig().UrlPfsense+"v1/interface/vlan", nil)
+	req.Header.Add("Authorization", functions.ViperReadConfig().ClientId + " "+functions.ViperReadConfig().ClientToken)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("response Status : ", resp.Status)
+	fmt.Println("response Headers : ", resp.Header)
+}
---
+++ cmd/_interface/create.go
@@ -0,0 +1,71 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package _interface
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/cobra"
+	"gitlab.com/nt-factory/2021/admin/pfcli/functions"
+	"gitlab.com/nt-factory/2021/admin/pfcli/models"
+	"log"
+	"net/http"
+)
+
+// createCmd represents the create command
+var CreateCmd = &cobra.Command{
+	Use:   "create",
+	Run: func(cmd *cobra.Command, args []string) {
+		createInterface()
+	},
+}
+
+func init() {
+	createFlag()
+}
+var Interface_Model models.Interface
+func createFlag()  {
+	pf := CreateCmd.PersistentFlags()
+	pf.StringVarP(&Interface_Model.If, "if", "i", "", models.InerfaceIfDescr)
+	pf.BoolVar(&Interface_Model.Enable, "enable",false, models.InerfaceEnableDescr)
+	pf.StringVarP(&Interface_Model.Descr, "descr", "d", "", models.Inerface_descr_Descr)
+	pf.StringVarP(&Interface_Model.Subnet, "subnet", "s", "", models.InerfaceSubnetDescr)
+	pf.StringVarP(&Interface_Model.Type, "type", "t", "", models.InerfaceTypeDescr)
+	pf.BoolVar(&Interface_Model.Blockbogons, "blockbogons",  true, models.InerfaceBlockbogonsDescr)
+}
+func createInterface()  {
+	jsonReq, _ := json.Marshal(Interface_Model)
+	res := functions.JsonOutput(jsonReq)
+	fmt.Println(res)
+	req, err := http.NewRequest("POST", functions.ViperReadConfig().UrlPfsense+"v1/interface", bytes.NewBuffer(jsonReq))
+	req.Header.Add("Authorization", functions.ViperReadConfig().ClientId + " "+functions.ViperReadConfig().ClientToken)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+
+	}
+
+	fmt.Println("response Status : ", resp.Status)
+	fmt.Println("response body : ", resp.Body)
+	fmt.Println("response Headers : ", resp.Header)
+}
